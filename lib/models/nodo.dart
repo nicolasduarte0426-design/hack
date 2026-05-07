bd70dca07b9433d977176111530a208d3a08b194
NarethWebDev <webdevnareth@gmail.com>
2026-05-07 12:15:29 -0500

    Se creó el modelo Nodo con nombre, mision y coordenadas

---
+++ lib/models/nodo.dart
@@ -0,0 +1,13 @@
+class Nodo {
+  final String nombre;
+  final String mision;
+  final double latitud;
+  final double longitud;
+
+  const Nodo({
+    required this.nombre,
+    required this.mision,
+    required this.latitud,
+    required this.longitud,
+  });
+}
